9a69cc25c1bf454146b5869b9ce105a68ef584f0
John Doe <125364972+Elbowg@users.noreply.github.com>
2025-10-02 13:41:26 +0200

    Test to see if backend works

---
+++ Backend/backend/src/main/java/com/clims/backend/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.clims.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/api/hello";
+    }
+}
